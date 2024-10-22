5ae3fb5425852ecdbbdeb4865b6cb282d4eb2d6f
Vincent Lafeychine <vincent.lafeychine@proton.me>
2024-10-22 17:27:00 +0200

    feat(ci): Add build pipeline using nix

--- flake.nix
+++ flake.nix
@@ -4,39 +4,38 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    flake-utils,
-    nixpkgs,
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-      coqPackages = pkgs.coqPackages_8_19;
-    in rec {
-      packages = {
-        theories = coqPackages.mkCoqDerivation rec {
-          pname = "FormArith";
-          opam-name = "FormArith";
-          version = "0.1.0";
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        coqPackages = pkgs.coqPackages_8_19;
+      in
+      rec {
+        packages = {
+          theories = coqPackages.mkCoqDerivation rec {
+            pname = "FormArith";
+            opam-name = "FormArith";
+            version = "0.1.0";
 
-          src = ./.;
-          useDune = true;
+            src = ./.;
+            useDune = true;
+          };
 
-          installPhase = ''
-            runHook preInstall
-            dune install --prefix $out ${opam-name}
-            runHook postInstall
-          '';
+          default = packages.theories;
         };
 
-        default = packages.theories;
-      };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = with packages; [ theories ];
+          packages = with coqPackages; [ vscoq-language-server ];
+        };
 
-      devShells.default = pkgs.mkShell {
-        inputsFrom = with packages; [theories];
-        packages = with coqPackages; [vscoq-language-server];
-      };
-
-      formatter = pkgs.alejandra;
-    });
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
