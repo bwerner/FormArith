e69cc9fab40686acd58e1a4349a864451dfe1725
Vincent Lafeychine <vincent.lafeychine@proton.me>
2024-10-22 17:03:10 +0200

    feat(nix): Add build and develop

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    flake-utils,
+    nixpkgs,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      coqPackages = pkgs.coqPackages_8_19;
+    in rec {
+      packages = {
+        theories = coqPackages.mkCoqDerivation rec {
+          pname = "FormArith";
+          opam-name = "FormArith";
+          version = "0.1.0";
+
+          src = ./.;
+          useDune = true;
+
+          installPhase = ''
+            runHook preInstall
+            dune install --prefix $out ${opam-name}
+            runHook postInstall
+          '';
+        };
+
+        default = packages.theories;
+      };
+
+      devShells.default = pkgs.mkShell {
+        inputsFrom = with packages; [theories];
+        packages = with coqPackages; [vscoq-language-server];
+      };
+
+      formatter = pkgs.alejandra;
+    });
+}
