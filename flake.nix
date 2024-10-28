09961fec853dca487d5224fb93853bc5f38b2bd3
Vincent Lafeychine <vincent.lafeychine@proton.me>
2024-10-28 23:35:17 +0100

    chore(nix): Add autosubst dependency

--- flake.nix
+++ flake.nix
@@ -18,13 +18,28 @@
       in
       rec {
         packages = {
-          theories = coqPackages.mkCoqDerivation rec {
+          theories = let
+            autosubst = coqPackages.mkCoqDerivation {
+              pname = "autosubst";
+              version = "1.9";
+
+              # The tarball is not present in the release
+              src = pkgs.fetchFromGitHub {
+                owner = "coq-community";
+                repo = "autosubst";
+                rev = "v1.9";
+                sha256 = "sha256-XiLZjMc+1iwRGOstfLm/WQRF6FTdX6oJr5urn3wmLlA=";
+              };
+            };
+          in coqPackages.mkCoqDerivation rec {
             pname = "FormArith";
             opam-name = "FormArith";
             version = "0.1.0";
 
             src = ./.;
             useDune = true;
+
+            propagatedBuildInputs = [ autosubst ];
           };
 
           default = packages.theories;
