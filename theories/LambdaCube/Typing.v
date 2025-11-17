9b5d5b19fbaf025b3b0b035cc0bec386b2446dd7
Bousmaha Wassel <wassel2005@gmail.com>
2025-11-17 15:00:16 +0100

    typing contexts are now parametrized by one integer and not 2

--- theories/LambdaCube/Typing.v
+++ theories/LambdaCube/Typing.v
@@ -3,39 +3,26 @@ From Coq Require Import Arith Lia.
 From FormArith Require Import Base.
 From FormArith.LambdaCube Require Import Term Operational.
 
-Inductive typing_ctx n : nat -> Type :=
-| empty : typing_ctx n 0
-| cons {l} : typing_ctx n l -> term n -> typing_ctx n (S l).
-
-Arguments empty {n}.
-Arguments cons {n l}.
+Inductive typing_ctx : nat -> Type :=
+| empty : typing_ctx 0
+| cons {n} : typing_ctx n -> term n -> typing_ctx (S n).
 
 Notation "\" := empty.
+Notation "A , t" := (cons A t) (at level 61, left associativity, t at next level).
 
-Fixpoint kth_index_ctx {n l} (Γ: typing_ctx n l) k {struct Γ} : k < l -> term n :=
-  match Γ in typing_ctx _ l return k < l -> term n with
+Search (_ < S _ -> {_} + {_}).
+
+Fixpoint kth_index_ctx {n} (Γ: typing_ctx n) k {struct Γ} : k < n -> term n :=
+  match Γ in typing_ctx n return k < n -> term n with
   | \ => fun H => match Nat.nlt_0_r _ H with end
-  | @cons _ l Γ t =>
-      match k return k < S l -> term n with
-      | 0 => fun _ => t
-      | S k => fun (H: S k < S l) => kth_index_ctx Γ k (le_S_n _ _ H)
-      end
-  end
-.
-
-Fixpoint map_ctx {n m l} (f: term n -> term m) (Γ: typing_ctx n l) : typing_ctx m l :=
-  match Γ with
-  | \ => \
-  | cons Γ t => cons (map_ctx f Γ) (f t)
+  | @cons n Γ t =>
+      fun _ =>
+        ren weaken
+          match lt_dec k n with
+          | left H => kth_index_ctx Γ k H
+          | right _ => t
+          end
   end.
-
-Definition slide {n} (i : {k | k < n}) : {k | k < S n} :=
-  exist _ (S (proj1_sig i)) (le_n_S _ _ (proj2_sig i)).
-
-Definition cons' {n} (Γ : typing_ctx n n) (t: term n) : typing_ctx (S n) (S n) :=
-  cons (map_ctx (ren slide) Γ) (ren slide t).
-
-Notation "A , t" := (cons' A t) (at level 61, left associativity, t at next level).
 
 Definition pi_scheme := sort -> sort -> bool.
 
@@ -45,25 +32,25 @@ Reserved Notation "A ⊢( P ) s ':' t"
   (at level 70, s at next level, t at next level, P at next level).
 Reserved Notation "A ⊢( P )" (at level 70, P at next level).
 
-Inductive typing (P: pi_scheme) : forall {n}, typing_ctx n n -> term n -> term n -> Prop :=
-| typ_star {n} {Γ: typing_ctx n n} : Γ ⊢(P) -> Γ ⊢(P) Srt Star : Srt Box
-| typ_var {n} {Γ: typing_ctx n n} {i} :
+Inductive typing (P: pi_scheme) : forall {n}, typing_ctx n -> term n -> term n -> Prop :=
+| typ_star {n} {Γ: typing_ctx n} : Γ ⊢(P) -> Γ ⊢(P) Srt Star : Srt Box
+| typ_var {n} {Γ: typing_ctx n} {i} :
   Γ ⊢(P) -> Γ ⊢(P) Var i : kth_index_ctx Γ (proj1_sig i) (proj2_sig i)
-| typ_pi {n} {Γ: typing_ctx n n} {A B s s'} :
+| typ_pi {n} {Γ: typing_ctx n} {A B s s'} :
   pi_scheme_check P s s' -> Γ ⊢(P) A : Srt s -> Γ, A ⊢(P) B : Srt s' -> Γ ⊢(P) Pi A B : Srt s'
-| typ_abs {n} {Γ: typing_ctx n n} {A B s s' t} :
+| typ_abs {n} {Γ: typing_ctx n} {A B s s' t} :
   pi_scheme_check P s s' ->
   Γ ⊢(P) A : Srt s ->
   Γ, A ⊢(P) B : Srt s' ->
   Γ, A ⊢(P) t : B ->
   Γ ⊢(P) Abs A t : Pi A B
-| typ_app {n} {Γ: typing_ctx n n} {A B t u} :
+| typ_app {n} {Γ: typing_ctx n} {A B t u} :
   Γ ⊢(P) t : Pi A B -> Γ ⊢(P) u : A -> Γ ⊢(P) App t u : bind (bind_first u) B
-| typ_conv {n} {Γ: typing_ctx n n} {t A B} :
+| typ_conv {n} {Γ: typing_ctx n} {t A B} :
   Γ ⊢(P) t : A -> A ≅ B -> Γ ⊢(P) t : B
-with ctx_wf (P: pi_scheme) : forall {n}, typing_ctx n n -> Prop :=
+with ctx_wf (P: pi_scheme) : forall {n}, typing_ctx n -> Prop :=
 | empty_wf : \ ⊢(P)
-| cons_wf {n} {A: typing_ctx n n} {t: term n} {s} :
+| cons_wf {n} {A: typing_ctx n} {t: term n} {s} :
   A ⊢(P) -> A ⊢(P) t : Srt s -> A, t ⊢(P)
 where "A ⊢( P ) t ':' s" := (typing P A t s)
 and "A ⊢( P )" := (ctx_wf P A)
